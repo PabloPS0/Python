eb788464335a9a977fe8df87834dad81544218ff
PabloPS0 <pablopierry.github.98@gmail.com>
2023-02-05 02:01:43 -0300

    Added function to compare if strings are equal

--- detector_palindromo.py
+++ detector_palindromo.py
@@ -6,4 +6,7 @@ Inverso = ""
 for letra in range(len(StringCaseFold)-1, -1, -1): #Ex: palavra com 15 letras = 15-1 = 14, -1, -1 -> conta da letra 14 até a 0
     Inverso += StringCaseFold #Inverso = Inverso + StringCaseFold
 
-if Inverso == 
+if Inverso == StringCaseFold:
+    print(f"A frase ({String.upper()}) é um Palíndromo.")
+else: 
+    print(f"A frase ({String.upper()}) Não é um Palíndromo.")
