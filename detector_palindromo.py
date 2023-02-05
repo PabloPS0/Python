997fe5ecca2f412e8d88309b917b8dabe5d9c2e7
PabloPS0 <pablopierry.github.98@gmail.com>
2023-02-05 01:57:31 -0300

    First commit

---
+++ detector_palindromo.py
@@ -0,0 +1,9 @@
+String = str(input('Digite uma Frase: '))
+StringSemEspaços = String.replace(' ', '')
+StringCaseFold = StringSemEspaços.casefold()
+Inverso = ""
+
+for letra in range(len(StringCaseFold)-1, -1, -1): #Ex: palavra com 15 letras = 15-1 = 14, -1, -1 -> conta da letra 14 até a 0
+    Inverso += StringCaseFold #Inverso = Inverso + StringCaseFold
+
+if Inverso == 
