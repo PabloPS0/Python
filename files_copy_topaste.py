64714b9fe9836e04e883ab4973c662b42785ea95
PabloPS0 <pablopierry.github.98@gmail.com>
2023-02-18 17:22:31 -0300

    Modification of folder path

--- files_copy_topaste.py
+++ files_copy_topaste.py
@@ -1,9 +1,10 @@
 import os # Módulo que traz informações sobre o sistema operacional
 import shutil # Módulo útil para mexer com arquivos (copiar e colar)
 import psutil # Módulo útil para implementar ferramentas de monitoramento de sistema e otimização de desempenho em programas
+from time import sleep
 
-folder_path = "EsteComputador/S.O(E:)/Music"
-temp_folder_path = "EsteComputador/S.O(E:)/temp_folder_path"
+folder_path = "E:/Music/16gb"
+temp_folder_path = "E:/Music/temp_folder_path"
 
 def get_files(folder_path): # Função para listar os arquivos do diretório
     files = os.listdir(folder_path)
@@ -22,7 +23,13 @@ def is_valid_drives(): # Função para verificar se os drives são válidos para
     if os.path.exist(drives):
         return True
     return False
-def send_fles(temp_folder_path, valid_drives): # Função para enviar os arquivos para os dispositivos externos válidos
+def send_files(temp_folder_path, valid_drives): # Função para enviar os arquivos para os dispositivos externos válidos
     for drive in valid_drives:
         shutil.move(temp_folder_path, os.path.join(drive, "new folder"))
 while True: # Loop que executa todas essas funções periodicamente para verificar se há novos arquivos no diretório e dispositivos externos conectados
+    files = get_files(folder_path)
+    copy_files(files, temp_folder_path)
+    drive = get_external_drives()
+    valid_drives = [drive for drive in drive if is_valid_drives(drive)]
+    send_files(temp_folder_path, valid_drives)
+    sleep(60) # 60s de tempo de espera para execução de uma nova verificação (Loop)
