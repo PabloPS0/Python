57f360a5975a721eab182188f2df7d1d43988d8d
PabloPS0 <pablopierry.github.98@gmail.com>
2023-02-18 16:26:17 -0300

    Creation of functions to list, copy and paste files and detect removable devices connected to the computer

--- files_copy_topaste.py
+++ files_copy_topaste.py
@@ -2,4 +2,16 @@ import os # Módulo que traz informações sobre o sistema operacional
 import shutil # Módulo útil para mexer com arquivos (copiar e colar)
 import psutil # Módulo útil para implementar ferramentas de monitoramento de sistema e otimização de desempenho em programas
 
+folder_path = "EsteComputador/S.O(E:)/Music"
+temp_folder_path = "EsteComputador/S.O(E:)/temp_folder_path"
+
+def get_files(folder_path): # Função para listar os arquivos do diretório
+    files = os.listdir(folder_path)
+    return [os.path.join(folder_path, f) for f in files]
+
+def copy_files(files, temp_folder_path): # Função para copiar os arquivos do diretório para a pasta temporária
+    for f in files:
+        shutil.copy(f, temp_folder_path)
+def get_external_drives(): # Função para detectar dispositivos externos ou removíveis conectados ao computador
+    drives = []
 
