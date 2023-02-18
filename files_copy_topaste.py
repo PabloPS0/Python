50b63964b88d99975bfefd7239b4cac7bd0f35a4
PabloPS0 <pablopierry.github.98@gmail.com>
2023-02-18 15:33:03 -0300

    Module import

---
+++ files_copy_topaste.py
@@ -0,0 +1,5 @@
+import os # Módulo que traz informações sobre o sistema operacional
+import shutil # Módulo útil para mexer com arquivos (copiar e colar)
+import psutil # Módulo útil para implementar ferramentas de monitoramento de sistema e otimização de desempenho em programas
+
+
