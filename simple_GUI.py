46f5b3a89e0147c26dce9582ef39e46fa6a91e8b
PabloPS0 <pablopierry.github.98@gmail.com>
2023-02-19 02:43:45 -0300

    creation of a function to create a window where the content will remain

--- simple_GUI.py
+++ simple_GUI.py
@@ -1,9 +1,15 @@
 import PySimpleGUI as sg
-sg.theme('DarkAmber')
 
+sg.theme('DarkAmber') # Cor do tema
+ 
+# Criar Todo o Conteúdo que Terá Dentro da Janela
 layout = [  [sg.Text('Nome')], 
             [sg.Text('Idade')],
+            [sg.Text('Empregado'), sg.Button('Sim'), sg.Button('Não')]
             [sg.Button('Enviar'), sg.Button('Cancelar')]
 ]
 
 # Criar uma Janela
+window = sg.Window('Cadastro', layout)
+
+# Evento de Loop para processar "eventos" e obter os "valores" da entrada de dados
